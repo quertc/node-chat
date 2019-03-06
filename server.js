81367b41a7261766e66ad7e19356391c2990bcb5
quertc <fray-1980@mail.ru>
2019-03-06 14:29:19 +0300

    Creating a chat

---
+++ server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app).listen(3000);
+let io = require('socket.io').listen(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
+})
+
+let connections = [];
+
+io.sockets.on('connection', (socket) => {
+  connections.push(socket);
+  console.info(`Пользователь ${connections.indexOf(socket) + 1} подключен`)
+
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.info('Пользователь отключен')
+  })
+
+	socket.on('send mess', (data) => {
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+})
