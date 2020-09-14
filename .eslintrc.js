6f7fd0cb744944ffc4888e96f754779d6ca509cc
Artemy <48246993+quertc@users.noreply.github.com>
2020-09-14 19:05:55 +0300

    Updated dependencies and installed ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    'arrow-parens': ['error', 'as-needed'],
+  },
+};
